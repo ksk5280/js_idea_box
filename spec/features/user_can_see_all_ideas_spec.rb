de8d08936b6b653077fb36ed168735eb523dcb28
Kimiko Kano <kskano@gmail.com>
2016-05-09 11:22:31 -0600

    Add feature test: user can see all ideas

---
+++ spec/features/user_can_see_all_ideas_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Use can see all ideas" do
+  scenario "they see title, body, and quality for each idea" do
+    idea1 = Idea.create(title: "Idea title", body: "Idea body", quality: "genius")
+    idea2 = Idea.create(title: "Idea title 2", body: "Idea body 2")
+    visit "/"
+
+    expect(page).to have_content "Idea title"
+    expect(page).to have_content "Idea body"
+    expect(page).to have_content "genius"
+
+    expect(page).to have_content "Idea title 2"
+    expect(page).to have_content "Idea body 2"
+    expect(page).to have_content "swill"
+  end
+end
