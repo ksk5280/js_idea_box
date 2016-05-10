b16c6b5bfda6cd127859870f51309d2d199a125d
Kimiko Kano <kskano@gmail.com>
2016-05-10 16:17:33 -0600

    Idea bodies are truncated to the nearest word when over 100 characters

--- spec/features/user_can_see_all_ideas_spec.rb
+++ spec/features/user_can_see_all_ideas_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "Use can see all ideas" do
   scenario "they see title, body, and quality for each idea" do
     idea1 = Idea.create(title: "Idea title", body: "Idea body", quality: "genius")
     idea2 = Idea.create(title: "Idea title 2", body: "Idea body 2", quality: "swill")
+
     visit "/"
 
     expect(page).to have_content "Idea title"
@@ -13,5 +14,19 @@ RSpec.feature "Use can see all ideas" do
     expect(page).to have_content "Idea title 2"
     expect(page).to have_content "Idea body 2"
     expect(page).to have_content "swill"
+  end
+
+  scenario "they see a truncated body if it's longer than 100 characters" do
+    idea = Idea.create(title: "Idea title", body: "This is a really long body "\
+      "for my great idea that I am super excited to share with the world. "\
+      "I think that everyone is going to really like this great idea!")
+
+    visit "/"
+
+    expected = "This is a really long body for my great idea that I am super "\
+    "excited to share with the world. I..."
+
+    expect(page).to have_content "Idea title"
+    expect(page).to have_content expected
   end
 end
