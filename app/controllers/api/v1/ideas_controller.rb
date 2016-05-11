6f1d835a859af05c71c1d73c7ccc8a4f913bb6a5
Kimiko Kano <kskano@gmail.com>
2016-05-10 18:39:53 -0600

    Add form for new idea.
    * Add feature test for creating new idea.
    * Add route for creating new idea.

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::IdeasController < ApiController
   def index
     respond_with Idea.all
   end
+
+  def create
+    respond_with Idea.create(idea_params)
+  end
+
+  private
+
+    def idea_params
+      params.permit(:title, :body)
+    end
 end
---
+++ spec/features/user_can_add_ideas_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "User can add ideas" do
+  scenario "they see new idea on page" do
+    visit "/"
+
+    fill_in "Title", with: "Idea Title"
+    fill_in "Body", with: "Idea Body"
+    click_button "Save"
+
+    within ".ideas" do
+      expect(page).to have_content "Idea Title"
+      expect(page).to have_content "Idea Body"
+    end
+  end
+end
