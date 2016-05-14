c5b57fc2c16594e32d1bd750f6e6a0f82d2bb446
Kimiko Kano <kskano@gmail.com>
2016-05-14 16:37:28 -0600

    Add controller test for delete Delete sprintf cdn

--- spec/requests/api/v1/ideas_controller_spec.rb
+++ spec/requests/api/v1/ideas_controller_spec.rb
@@ -13,8 +13,8 @@ RSpec.describe "GET api/v1/ideas" do
   end
 
   it "shows all ideas" do
-    idea1 = Idea.create(title: "Idea title 1", body: "Idea body 1")
-    idea2 = Idea.create(title: "Idea title 2", body: "Idea body 2")
+    Idea.create(title: "Idea title 1", body: "Idea body 1")
+    Idea.create(title: "Idea title 2", body: "Idea body 2")
     get "/api/v1/ideas"
 
     expect(json_response[0]["title"]).to eq "Idea title 1"
@@ -35,7 +35,6 @@ RSpec.describe "GET api/v1/ideas" do
 
   it "responds with errors if idea title is missing" do
     params = { body: "Idea body" }
-
     post "/api/v1/ideas", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 
     expect(json_response["errors"]).to eq({"title"=>["can't be blank"]})
@@ -43,7 +42,6 @@ RSpec.describe "GET api/v1/ideas" do
 
   it "responds with errors if idea body is missing" do
     params = { title: "Idea title" }
-
     post "/api/v1/ideas", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 
     expect(json_response["errors"]).to eq({"body"=>["can't be blank"]})
@@ -51,16 +49,14 @@ RSpec.describe "GET api/v1/ideas" do
 
   it "responds with errors if title and body are missing" do
     params = { }
-
     post "/api/v1/ideas", params.to_json, { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
     expect(json_response["errors"]).to eq({"title"=>["can't be blank"], "body"=>["can't be blank"]})
   end
 
   it "can update ideas" do
-    idea = Idea.create(title: "Idea title", body: "Idea body")
-
+    Idea.create(title: "Idea title", body: "Idea body")
     params = { title: "New title", body: "New body" }
-
     post "/api/v1/ideas", params.to_json, { method: :update, 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 
     expect(json_response["title"]).to eq "New title"
@@ -68,10 +64,18 @@ RSpec.describe "GET api/v1/ideas" do
   end
 
   it "responds with errors if idea title and body are blank when updating" do
+    idea = Idea.create(title: "Idea title", body: "Idea body")
     params = { title: "", body: "" }
-
     post "/api/v1/ideas", params.to_json, { method: :update, 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
 
     expect(json_response["errors"]).to eq({"title"=>["can't be blank"], "body"=>["can't be blank"]})
+  end
+
+  it "can delete ideas" do
+    idea = Idea.create(title: "Idea title", body: "Idea body")
+    expect(Idea.count).to eq 1
+
+    delete "/api/v1/ideas/#{idea.id}"
+    expect(Idea.count).to eq 0
   end
 end
