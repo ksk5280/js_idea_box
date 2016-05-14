2e146280792f5dac1a6ac0d9657b8c9429fbfef5
Kimiko Kano <kskano@gmail.com>
2016-05-13 23:23:05 -0600

    Add unit tests for quality changes

--- spec/javascripts/exampleTest_spec.js
+++ spec/javascripts/exampleTest_spec.js
@@ -1,5 +1,25 @@
-describe('new test', function(){
-  it('does a thing', function(){
-    assert(true);
-  })
-})
+describe('increaseQuality', function() {
+  it('increases Quality of idea up to genius', function() {
+    var quality = "swill";
+    assert.equal(increaseQuality(quality), "plausible");
+
+    quality = "plausible";
+    assert.equal(increaseQuality(quality), "genius");
+
+    quality = "genius";
+    assert.equal(increaseQuality(quality), "genius");
+  });
+});
+
+describe('decreaseQuality', function() {
+  it('decreases quality of idea down to swill', function() {
+    var quality = "genius";
+    assert.equal(decreaseQuality(quality), "plausible");
+
+    quality = "plausible";
+    assert.equal(decreaseQuality(quality), "swill");
+
+    quality = "swill";
+    assert.equal(decreaseQuality(quality), "swill");
+  });
+});
